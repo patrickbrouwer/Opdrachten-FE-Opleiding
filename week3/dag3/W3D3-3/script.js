f547acfd9cd1b1684702595dfc76e7f40275adfc
Patrick Brouwer <patrick@patrickbrouwer.eu>
2020-02-19 15:40:14 +0100

    First Commit

---
+++ week3/dag3/W3D3-3/script.js
@@ -0,0 +1,8 @@
+//getData();
+
+const doSomethingWithData = async function() {
+  const data = await getData();
+  console.log("here is your data to do something:", data);
+};
+
+doSomethingWithData();
